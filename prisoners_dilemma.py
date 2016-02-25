5f0e98560a6ac5b419771a93e4b3db109163957a
FinalFunction <Rdude8888@gmail.com>
2016-02-25 13:16:08 -0500

    Team 10 Ryan and Jack

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -364,22 +364,23 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
 
 
 
-
-    ######
-    ######
-    #
+    # FINAL VERSION
+    # TEAM TEN WOOOOO 
     elif player == 10:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'Ides of March'
         else:
             # use history, opponent_history, score, opponent_score
-            # to compute your strategy
+            # to compute your strategy      
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
             else:
-                return 'c' #otherwise collude
+                # AVERAGE choice of the opponent and us, colluding or betraying
+                opponent_average = opponent_history.count('c')/len(opponent_history)
+                if opponent_average >= 0.5:  # has a history of colluding
+                    return 'c'  
+                else: # has a history of betraying
+                    return 'b'
 
 
 
